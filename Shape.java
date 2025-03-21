714dc4b3937b12fe396304e93f350fd8d32f5d9c
gilad23099 <gw2309@gmail.com>
2025-03-21 23:25:44 +0200

    Fourth commit from main branch

--- Shape.java
+++ Shape.java
@@ -8,4 +8,11 @@ public abstract class Shape {
     public int getY(){
         return this.y;
     }
+    public void setX(int x){
+        this.x=x;
+    }
+    public void setY(int y){
+        this.y=y;
+    }
+
 }
